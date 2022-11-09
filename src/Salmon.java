931e131d729c1c5b83c4dc54ac7cb9773112381e
nielsencaroline <nielsenc@wlhs.wlwv.k12.or.us>
2022-11-09 09:28:39 -0800

    Edited CODEHS 3.4.7 with &&

--- src/Salmon.java
+++ src/Salmon.java
@@ -14,29 +14,25 @@ I want you to make a new version using logical operators
 */
 
 public class Salmon {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // Ask user for month of year as an integer
         System.out.println("Enter the month of the year as a number: ");
         int month = input.nextInt();
+
         // Use if/else if/else statement to determine if it is spawning season
-        if(month<3){
-            System.out.println("Not spawning season");
-        }
-        else if(month < 7){
+
+        if(month >= 3 && month <= 6){
             System.out.println("Spring spawning season");
         }
-        else if(month < 9){
-            System.out.println("Not spawning season");
-        }
-        else if(month < 12){
+
+        else if(month >= 9 && month <= 11){
             System.out.println("Fall spawning season");
         }
+
         else{
             System.out.println("Not spawning season");
         }
-
     }
 }
